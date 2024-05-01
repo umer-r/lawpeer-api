9ca8ea023e717550ba8980507692414ab09247a9
umer-r <umer-r@outlook.com>
2024-05-01 12:19:38 +0500

    Update: access control decorators added

--- api/routes/admin/urls.py
+++ api/routes/admin/urls.py
@@ -3,30 +3,34 @@
         Blueprint for Admin related routes.
         
     TODO:   1 - Add JWT logic secure on all routes (Only admins can access these routes).   - [DONE]
-            2 - Add DOC strings on each func.                                               - []
+            2 - Add DOC strings on each func.                                               - [DONE]
             3 - Implement Super admin logic.                                                - [DONE]
             4 - Test each of the following routes.                                          - [DONE]
             5 - Omit sensitive fields.                                                      - []
+            6 - Move access control to decorators.                                          - [DONE]
+            7 - Move generate_access_token to utils.                                        - [DONE]
 """
 
 # Lib Imports
 from flask import Blueprint, jsonify, request
-from flask_jwt_extended import jwt_required, get_jwt_identity
+from flask_jwt_extended import jwt_required
 from flasgger import swag_from
 
 # Module Imports
 from api.routes.admin.controllers import create_admin, update_admin, delete_admin, get_admin_by_id, get_all_admin
-from api.routes.admin.auth_controllers import generate_access_token, check_admin, check_current_admin, check_super_and_current_admin, check_super_admin
+from api.utils.token_generator import generate_admin_access_token
 from api.utils.status_codes import Status
-from api.utils.decorators import check_mandatory
+from api.decorators.mandatory_keys import check_mandatory
+from api.decorators.access_control_decorators import admin_required, super_admin_required, super_or_current_admin_required
 
 # ----------------------------------------------- #
 
 admin_routes = Blueprint('admin', __name__)
 
 @admin_routes.route('/', methods=['POST'])
-@jwt_required()
 @swag_from(methods=['POST'])
+@jwt_required()
+@super_admin_required
 @check_mandatory(['email', 'password'])
 def create_new_admin():
     """
@@ -60,12 +64,7 @@ def create_new_admin():
       409:
         description: Conflict. Admin with the same email already exists.
     """
-    
-    # Only (super) Admin can access:
-    is_super_admin = check_super_admin(get_jwt_identity())
-    if not is_super_admin:
-        return jsonify({'message': 'Unauthorized access'}), Status.HTTP_401_UNAUTHORIZED
-    
+        
     data = request.get_json()
     email = data.get('email')
     password = data.get('password')
@@ -80,6 +79,7 @@ def create_new_admin():
 @admin_routes.route('/', methods=['GET'])
 @jwt_required()
 @swag_from(methods=['GET'])
+@admin_required
 def all_admins():
     """
     Endpoint to retrieve all admins.
@@ -99,11 +99,6 @@ def all_admins():
         description: No admin found.
     """
     
-    # Only (all) Admins can access
-    is_admin = check_admin(get_jwt_identity())
-    if not is_admin:
-        return jsonify({'message': 'Unauthorized access'}), Status.HTTP_401_UNAUTHORIZED
-    
     admins = get_all_admin()
     if admins:
         return jsonify([admin.to_dict() for admin in admins]), Status.HTTP_200_OK
@@ -113,6 +108,7 @@ def all_admins():
 @admin_routes.route('/<int:id>', methods=['GET'])
 @jwt_required()
 @swag_from(methods=['GET'])
+@admin_required
 def get_admin(id):
     """
     Endpoint to retrieve admin details by admin ID.
@@ -137,12 +133,7 @@ def get_admin(id):
       404:
         description: Admin not found.
     """
-    
-    # Only (all) Admins can access
-    is_admin = check_admin(get_jwt_identity())
-    if not is_admin:
-        return jsonify({'message': 'Unauthorized access'}), Status.HTTP_401_UNAUTHORIZED
-    
+        
     admin = get_admin_by_id(id)
     if admin:
         return jsonify(admin.to_dict()), Status.HTTP_200_OK
@@ -152,6 +143,7 @@ def get_admin(id):
 @admin_routes.route('/<int:id>', methods=['PUT'])
 @jwt_required()
 @swag_from(methods=['PUT'])
+@super_or_current_admin_required
 def update_existing_admin(id):
     """
     Endpoint to update an existing admin by admin ID.
@@ -184,12 +176,7 @@ def update_existing_admin(id):
       404:
         description: Admin not found.
     """
-    
-    # Admin Should be the same as the requested admin.
-    is_requested_admin_or_super = check_super_and_current_admin(admin=get_jwt_identity(), id=id)
-    if not is_requested_admin_or_super:
-        return jsonify({'message': 'Unauthorized access'}), Status.HTTP_401_UNAUTHORIZED
-    
+        
     data = request.get_json()
     updated_admin = update_admin(id, **data)
     if updated_admin:
@@ -200,6 +187,7 @@ def update_existing_admin(id):
 @admin_routes.route('/<int:id>', methods=['DELETE'])
 @jwt_required()
 @swag_from(methods=['DELETE'])
+@super_or_current_admin_required
 def delete_existing_admin(id):
     """
     Endpoint to delete an existing admin by admin ID.
@@ -224,11 +212,6 @@ def delete_existing_admin(id):
       404:
         description: Admin not found.
     """
-    
-    # Admin Should be the same as the requested admin.
-    is_requested_admin_or_super = check_super_and_current_admin(admin=get_jwt_identity(), id=id)
-    if not is_requested_admin_or_super:
-        return jsonify({'message': 'Unauthorized access'}), Status.HTTP_401_UNAUTHORIZED
     
     deleted_admin = delete_admin(id)
     if deleted_admin:
@@ -278,7 +261,7 @@ def login():
     password = data.get('password')
 
     # Check if Admin is authorized (credentials correct) & generate the access token.
-    is_authorized, token = generate_access_token(email=email, password=password)
+    is_authorized, token = generate_admin_access_token(email=email, password=password)
     if is_authorized:
         return jsonify(access_token=token), Status.HTTP_200_OK
     
