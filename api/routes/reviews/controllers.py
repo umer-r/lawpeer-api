e09137fd49b90ea23295cea0d32c3a4104c447ca
umer-r <umer-r@outlook.com>
2024-05-07 00:57:07 +0500

    Update: create_new_review is now associated with contract

--- api/routes/reviews/controllers.py
+++ api/routes/reviews/controllers.py
@@ -1,13 +1,17 @@
 from api.models.review import Review
+from api.models.contract import Contract
 from api.database import db
 
 
-def create_new_review(client_id, lawyer_id, rating, review_text):
-    new_review = Review( rating=rating, review_text=review_text, client_id=client_id, lawyer_id=lawyer_id)
-    db.session.add(new_review)
-    db.session.commit()
-    
-    return new_review
+def create_new_review(contract_id, client_id, lawyer_id, rating, review_text):
+    contract = Contract.query.get(contract_id)
+    if contract:
+        if contract.is_ended:
+            new_review = Review( rating=rating, review_text=review_text, client_id=client_id, lawyer_id=lawyer_id)
+            db.session.add(new_review)
+            db.session.commit()
+            return new_review
+    return None
 
 def get_all_reviews():
     return Review.query.all()
--- api/routes/reviews/urls.py
+++ api/routes/reviews/urls.py
@@ -5,14 +5,17 @@
 
 # Lib Imports:
 from flask import Blueprint, jsonify, request
-from flask_jwt_extended import jwt_required, get_jwt_identity
+from flask_jwt_extended import jwt_required
 
 # Controller Imports:
 from .controllers import create_new_review, get_all_reviews, get_review_by_id, delete_review_by_id, get_client_reviews, get_lawyer_reviews
 
+# Decorators Imports:
+from api.decorators.mandatory_keys import check_mandatory
+from api.decorators.access_control_decorators import user_or_admin_required, admin_required, client_required
+
 # Module Imports:
 from api.utils.status_codes import Status
-from api.utils.decorators import check_mandatory
 
 # ----------------------------------------------- #
 
@@ -21,18 +24,21 @@ review_routes = Blueprint('review', __name__, url_prefix='/reviews')
 # CREATE: Create a new review
 @review_routes.route('/', methods=['POST'])
 @jwt_required()
-@check_mandatory(['rating', 'review_text', 'lawyer_id'])
+@check_mandatory(['contract_id', 'rating', 'review_text', 'lawyer_id'])
+@client_required
 def create_review():
     data = request.json
     
+    contract_id = data.get('contract_id')
     rating = data.get('rating')
     review_text = data.get('review_text')
     client_id = data.get('client_id')
     lawyer_id = data.get('lawyer_id')
     
-    create_new_review(client_id=client_id, lawyer_id=lawyer_id, rating=rating, review_text=review_text)
-    
-    return jsonify({'message': 'Review created successfully'}), Status.HTTP_201_CREATED
+    review = create_new_review(contract_id=contract_id, client_id=client_id, lawyer_id=lawyer_id, rating=rating, review_text=review_text)
+    if review:
+        return jsonify({'message': 'Review created successfully'}), Status.HTTP_201_CREATED
+    return jsonify({'message': 'Contract does not exists or is not ended yet.'}), Status.HTTP_400_BAD_REQUEST
 
 # READ-1: Retrieve all reviews
 @review_routes.route('/', methods=['GET'])
@@ -56,6 +62,7 @@ def get_review(review_id):
 
 # DELETE: delete review by id
 @review_routes.route('/<int:review_id>', methods=['DELETE'])
+@jwt_required()
 def delete_review(review_id):
     
     deleted_review = delete_review_by_id(review_id)
